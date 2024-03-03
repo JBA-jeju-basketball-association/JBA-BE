6625bfa457b9b45bd47c5c6c44afd34ee2967cdb
신한솔 <shinhansol@OneSol-MacBook.local>
2024-03-03 15:55:36 +0900

    first-commit

---
+++ src/main/java/github/com/jbabe/JbaBeApplication.java
@@ -0,0 +1,13 @@
+package github.com.jbabe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JbaBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JbaBeApplication.class, args);
+    }
+
+}
