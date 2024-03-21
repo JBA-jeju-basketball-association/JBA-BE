21846451ae6673e00db6576bcd94970d7f40cd11
sihu <sihu89@daum.net>
2024-03-21 13:43:46 +0900

    test: aws s3 test api 작성

--- src/main/java/github/com/jbabe/web/controller/Test.java
+++ src/main/java/github/com/jbabe/web/controller/Test.java
@@ -1,5 +1,8 @@
 package github.com.jbabe.web.controller;
 
+import com.amazonaws.HttpMethod;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.*;
 import github.com.jbabe.repository.redis.RedisTokenRepository;
 import github.com.jbabe.repository.user.User;
 import github.com.jbabe.repository.user.UserJpa;
@@ -12,10 +15,8 @@ import github.com.jbabe.web.dto.awsTest.PreSignedUploadInitiateRequest;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.net.URL;
@@ -29,13 +30,16 @@ import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
+import static io.lettuce.core.LettuceFutures.awaitAll;
+import static org.springframework.web.servlet.function.ServerResponse.async;
+
 @RestController
 @RequiredArgsConstructor
 public class Test {
 
     private final UserJpa userJpa;
     private final RedisTokenRepository redisTokenRepository;
-//    private final AmazonS3 amazonS3Client;
+    private final AmazonS3 amazonS3Client;
 
 
 //    public Test(UserJpa userJpa) {
@@ -58,52 +62,93 @@ public class Test {
         return "성공";
     }
 
-    //참고 https://techblog.woowahan.com/11392/
 
-//    @PostMapping("/initiate-upload")//업로드시작
-//    public InitiateMultipartUploadResult initiateUpload(@RequestBody PreSignedUploadInitiateRequest request) {
+    /*
+    우아한 기술 블로그 참고 https://techblog.woowahan.com/11392/
+    aws 멀티파트 , 또는 spring 멀티파트로 s3 버킷 사용⬇️
+     */
+    @PostMapping("/initiate-upload")//업로드시작
+    public InitiateMultipartUploadResult initiateUpload(@RequestBody PreSignedUploadInitiateRequest request) {
+        ObjectMetadata objectMetadata = new ObjectMetadata();
+        objectMetadata.setContentLength(request.getFileSize());
+        objectMetadata.setContentType(URLConnection.guessContentTypeFromName(request.getFileType()));
+
+        InitiateMultipartUploadRequest initiateRequest = new InitiateMultipartUploadRequest("bucketName", "objectName", objectMetadata);
+        return amazonS3Client.initiateMultipartUpload(initiateRequest);
+    }
+
+    @PostMapping("/presigned-url")//presigned 발급
+    public URL initiateUpload(@RequestBody PreSignedUrlCreateRequest request) {
+        LocalDateTime expirationTime = LocalDateTime.now().plusMinutes(10);
+        Date expiration = Date.from(expirationTime.atZone(ZoneId.systemDefault()).toInstant());
+
+        GeneratePresignedUrlRequest generatePresignedUrlRequest =
+                new GeneratePresignedUrlRequest("bucketName", "objectName")
+                        .withMethod(HttpMethod.PUT)
+                        .withExpiration(expiration);
+        generatePresignedUrlRequest.addRequestParameter("uploadId", request.getUploadId());
+        generatePresignedUrlRequest.addRequestParameter("partNumber", String.valueOf(request.getPartNumber()));
+
+        return amazonS3Client.generatePresignedUrl(generatePresignedUrlRequest);
+    }
+    @PostMapping("/complete-upload")//업로드완료
+    public CompleteMultipartUploadResult completeUpload(@RequestBody FinishUploadRequest finishUploadRequest) {
+        List<PartETag> partETags = finishUploadRequest.getParts().stream()
+                .map(part -> new PartETag(part.getPartNumber(), part.getETag()))
+                .collect(Collectors.toList());
+
+        CompleteMultipartUploadRequest completeMultipartUploadRequest = new CompleteMultipartUploadRequest(
+                "bucketName",
+                "objectName",
+                finishUploadRequest.getUploadId(),
+                partETags
+        );
+
+        return amazonS3Client.completeMultipartUpload(completeMultipartUploadRequest);
+    }
+    @PostMapping("/abort-upload")//업로드 취소
+    public void initiateUpload(@RequestBody PreSignedUrlAbortRequest request) {
+        AbortMultipartUploadRequest abortMultipartUploadRequest =
+                new AbortMultipartUploadRequest("bucketName", "objectName", request.getUploadId());
+        amazonS3Client.abortMultipartUpload(abortMultipartUploadRequest);
+    }
+    //⬆️aws sdk 사용
+    //⬇️멀티파트 방식
+//    @PostMapping("/multipart-files")//단일호출 한개 업로드
+//    public void uploadMultipleFile(@RequestPart MultipartFile file) throws IOException {
 //        ObjectMetadata objectMetadata = new ObjectMetadata();
-//        objectMetadata.setContentLength(request.getFileSize());
-//        objectMetadata.setContentType(URLConnection.guessContentTypeFromName(request.getFileType()));
+//        objectMetadata.setContentType(file.getContentType());
+//        objectMetadata.setContentLength(file.getSize());
 //
-//        InitiateMultipartUploadRequest initiateRequest = new InitiateMultipartUploadRequest("bucketName", "objectName", objectMetadata);
-//        return amazonS3Client.initiateMultipartUpload(initiateRequest);
-//    }
-//
-//    @PostMapping("/presigned-url")//presigned 발급
-//    public URL initiateUpload(@RequestBody PreSignedUrlCreateRequest request) {
-//        LocalDateTime expirationTime = LocalDateTime.now().plusMinutes(10);
-//        Date expiration = Date.from(expirationTime.atZone(ZoneId.systemDefault()).toInstant());
-//
-//        GeneratePresignedUrlRequest generatePresignedUrlRequest =
-//                new GeneratePresignedUrlRequest("bucketName", "objectName")
-//                        .withMethod(HttpMethod.PUT)
-//                        .withExpiration(expiration);
-//        generatePresignedUrlRequest.addRequestParameter("uploadId", request.getUploadId());
-//        generatePresignedUrlRequest.addRequestParameter("partNumber", String.valueOf(request.getPartNumber()));
-//
-//        return amazonS3Client.generatePresignedUrl(generatePresignedUrlRequest);
-//    }
-//    @PostMapping("/complete-upload")//업로드완료
-//    public CompleteMultipartUploadResult completeUpload(@RequestBody FinishUploadRequest finishUploadRequest) {
-//        List<PartETag> partETags = finishUploadRequest.getParts().stream()
-//                .map(part -> new PartETag(part.getPartNumber(), part.getETag()))
-//                .collect(Collectors.toList());
-//
-//        CompleteMultipartUploadRequest completeMultipartUploadRequest = new CompleteMultipartUploadRequest(
+//        PutObjectRequest putObjectRequest = new PutObjectRequest(
 //                "bucketName",
-//                "objectName",
-//                finishUploadRequest.getUploadId(),
-//                partETags
+//                "objectKey",
+//                file.getInputStream(),
+//                objectMetadata
 //        );
 //
-//        return amazonS3Client.completeMultipartUpload(completeMultipartUploadRequest);
+//        amazonS3Client.putObject(putObjectRequest);
 //    }
-//    @PostMapping("/abort-upload")//업로드 취소
-//    public void initiateUpload(@RequestBody PreSignedUrlAbortRequest request) {
-//        AbortMultipartUploadRequest abortMultipartUploadRequest =
-//                new AbortMultipartUploadRequest("bucketName", "objectName", request.getUploadId());
-//        amazonS3Client.abortMultipartUpload(abortMultipartUploadRequest);
-//    }
+    @PostMapping("/multipart-files")
+    public String uploadMultipleFiles(//여러개 업로드
+            @RequestPart("uploadFiles") List<MultipartFile> multipartFiles,
+            @RequestParam String type
+    ) throws IOException {
+        for (MultipartFile file : multipartFiles) {
+            ObjectMetadata objectMetadata = new ObjectMetadata();
+            objectMetadata.setContentType(file.getContentType());
+            objectMetadata.setContentLength(file.getSize());
+
+            PutObjectRequest putObjectRequest = new PutObjectRequest(
+                    "bucketName",
+                    "objectKey",
+                    file.getInputStream(),
+                    objectMetadata
+            );
+
+            amazonS3Client.putObject(putObjectRequest);
+        }
+        return "업로드 완료";
+    }
 
 }
