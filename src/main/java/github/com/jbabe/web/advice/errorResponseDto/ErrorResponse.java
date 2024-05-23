52d62b30d619f3a17371406fdd01e7956268e361
sihu <sihu89@daum.net>
2024-05-23 17:46:23 +0900

    feat:스웨거 다듬기 - 문서화 이후 타이틀과 설명 작성중 - 게시물 목록 조회 일부 로직 수정 #51

--- src/main/java/github/com/jbabe/web/advice/errorResponseDto/ErrorResponse.java
+++ src/main/java/github/com/jbabe/web/advice/errorResponseDto/ErrorResponse.java
@@ -1,10 +1,12 @@
 package github.com.jbabe.web.advice.errorResponseDto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
+@Schema(description = "공통 에러 응답 객체")
 public class ErrorResponse {
     private Integer code;
     private String message;
--- src/main/java/github/com/jbabe/web/dto/ResponseDto.java
+++ src/main/java/github/com/jbabe/web/dto/ResponseDto.java
@@ -1,6 +1,7 @@
 package github.com.jbabe.web.dto;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.http.HttpStatus;
@@ -8,9 +9,11 @@ import org.springframework.http.HttpStatus;
 @Getter
 @Setter
 public class ResponseDto {
+    @Schema(description = "응답 코드")
     private int code;
     private String message;
     @JsonInclude(JsonInclude.Include.NON_NULL)
+    @Schema(description = "응답 데이터")
     private Object data;
 
     // 생성자, 게터 및 세터는 필요에 따라 추가할 수 있습니다.
