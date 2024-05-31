5ac5b40641fd404dba682898c67e9f84288ae8ac
shinhansol <wkdqhrh010@naver.com>
2024-05-31 17:41:55 +0900

    #58 feat: 회원 관련 및 이메일 인증 Controller 에 swagger 적용 완료

--- src/main/java/github/com/jbabe/web/dto/authAccount/LoginRequest.java
+++ src/main/java/github/com/jbabe/web/dto/authAccount/LoginRequest.java
@@ -1,5 +1,6 @@
 package github.com.jbabe.web.dto.authAccount;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Pattern;
@@ -9,7 +10,9 @@ import lombok.Data;
 public class LoginRequest {
     @Email(message = "이메일 형식이 올바르지 않습니다.")
     @NotEmpty(message = "이메일을 입력해주세요.")
+    @Schema(description = "이메일", example = "hansol@gmail.com")
     private String email;
 
+    @Schema(description = "패스워드", example = "abc12341234!")
     private String password;
 }
