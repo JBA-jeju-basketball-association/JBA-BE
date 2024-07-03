9e44b94ee1f62b40deba31d84ec0f850608fde75
shinhansol <wkdqhrh010@naver.com>
2024-07-03 16:53:00 +0900

    #101 fix: 회원가입 로직 수정

--- src/main/java/github/com/jbabe/web/dto/authAccount/SignUpRequest.java
+++ src/main/java/github/com/jbabe/web/dto/authAccount/SignUpRequest.java
@@ -1,8 +1,15 @@
 package github.com.jbabe.web.dto.authAccount;
 
+import github.com.jbabe.repository.user.User;
+import github.com.jbabe.service.exception.ConflictException;
+import github.com.jbabe.service.exception.InvalidReqeustException;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.*;
 import lombok.Getter;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
 
 @Getter
 public class SignUpRequest {
@@ -30,21 +37,8 @@ public class SignUpRequest {
     @Schema(description = "휴대폰번호", example = "010-1234-5678")
     private String phoneNum;
 
-    @NotEmpty
-    @Schema(description = "성별", example = "MALE")
-    private String gender;
-
-    @Min(1900)
-    @Max(2024)
-    private Integer year;
-
-    @Min(1)
-    @Max(12)
-    private Integer month;
-
-    @Min(1)
-    @Max(31)
-    private Integer day;
+    @Schema(description = "주민번호 앞 7자리", example = "9602161")
+    private String birth;
 
     @NotEmpty
     @Schema(description = "소속팀", example = "제농회")
@@ -52,5 +46,36 @@ public class SignUpRequest {
 
     public boolean equalsPasswordAndPasswordConfirm() {
         return password.equals(passwordConfirm);
+    }
+
+    public LocalDate getBirthByLocalDate() {
+
+        String dateStr = birth.substring(0, 6);
+        LocalDate date;
+
+        // 추출한 6자리 문자열을 yyMMdd 형식의 LocalDate로 변환
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyMMdd");
+        try {
+             date = LocalDate.parse(dateStr, formatter);
+        }catch (Exception e) {
+            throw new InvalidReqeustException("주민번호 유효성 검사 실패", birth);
+        }
+        int currentYear = LocalDate.now().getYear();
+        if (date.getYear() > currentYear) {
+            date = date.withYear(date.getYear() - 100);
+        }
+
+        return date;
+    }
+
+    public User.Gender transformGender() {
+        String num = birth.substring(7, 8);
+        if (num.equals("1") || num.equals("3")) {
+            return User.Gender.MALE;
+        }else if (num.equals("2") || num.equals("4")){
+            return User.Gender.FEMALE;
+        }else {
+            throw new InvalidReqeustException("주민번호 유효성 검사 실패", birth);
+        }
     }
 }
