d3b613b7efd9b1f5f8660f101e4504d16ed828b6
shinhansol <wkdqhrh010@naver.com>
2024-08-30 17:16:38 +0900

    fix: getVideoList 신규순으로 정렬

--- src/main/java/github/com/jbabe/repository/video/VideoJpa.java
+++ src/main/java/github/com/jbabe/repository/video/VideoJpa.java
@@ -13,7 +13,8 @@ public interface VideoJpa extends JpaRepository<Video, Integer> {
     @Query("SELECT new github.com.jbabe.web.dto.video.GetVideoResponse(v.videoId, u.name, v.title, v.url, v.content, v.createAt) " +
             "FROM Video v " +
             "LEFT JOIN v.user u " +
-            "WHERE v.videoStatus = :status AND v.isOfficial = :isOfficial AND v.title LIKE %:keyword% ")
+            "WHERE v.videoStatus = :status AND v.isOfficial = :isOfficial AND v.title LIKE %:keyword% " +
+            "ORDER BY v.videoId DESC ")
     Page<GetVideoResponse> findAllByVideoStatusAndIsOfficialWithKeyword(Video.VideoStatus status, boolean isOfficial, String keyword, Pageable pageable);
 
     boolean existsByTitle(String title);
