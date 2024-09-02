d82a67147480b88a58997527b249b93e39f421cb
shinhansol <wkdqhrh010@naver.com>
2024-09-02 15:36:39 +0900

    fix: updatePasswordDto

--- src/main/java/github/com/jbabe/web/dto/authAccount/UpdatePasswordRequest.java
+++ src/main/java/github/com/jbabe/web/dto/authAccount/UpdatePasswordRequest.java
@@ -8,15 +8,15 @@ import lombok.Getter;
 
 @Getter
 public class UpdatePasswordRequest {
-    @NotBlank
+    @NotBlank(message = "비어있을 수 없습니다.")
     private String prevPW;
 
-    @NotBlank
+    @NotBlank(message = "비어있을 수 없습니다.")
     @Pattern(regexp="^(?=.*[a-zA-Z])(?=.*\\d)(?=.*\\W).{8,20}$", message = "비밀번호는 영문과 특수문자 숫자를 포함하며 8자 이상 20자 이하여야 합니다.")
     @Schema(description = "새 비밀번호", example = "abc123123!")
     private String newPW;
 
-    @NotBlank
+    @NotBlank(message = "비어있을 수 없습니다.")
     @Schema(description = "새 비밀번호 확인", example = "abc123123!")
     private String newPWConfirm;
 }
