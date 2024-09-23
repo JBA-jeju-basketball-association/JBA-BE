6383165e7c4dfa32c5babcfded6737321c0c88ee
shinhansol <wkdqhrh010@naver.com>
2024-09-23 11:33:10 +0900

    fix competition

--- src/main/java/github/com/jbabe/service/competition/CompetitionDetailService.java
+++ src/main/java/github/com/jbabe/service/competition/CompetitionDetailService.java
@@ -170,7 +170,7 @@ public class CompetitionDetailService {
         List<CompetitionImg> competitionImgs = competitionImgJpa.findAllByCompetition(competition);
         List<CompetitionAttachedFile> competitionAttachedFiles = competitionAttachedFileJpa.findAllByCompetition(competition);
         List<Division> divisions = divisionJpa.findAllByCompetition(competition);
-        if (competitionJpa.existsByCompetitionName(request.getTitle()))
+        if (!competition.getCompetitionName().equals(request.getTitle()) && competitionJpa.existsByCompetitionName(request.getTitle()))
             throw new ConflictException("Duplication CompetitionName", competition.getCompetitionName());
 
         //competition table update
